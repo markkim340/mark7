cfb163e12cf313caa3c8733c81ab78987629f5ec
mark_kim <mark340v@gmail.com>
2025-05-03 16:09:41 +0900

    fea: add login screen

--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mark7/common/component/custom_text_form_field.dart';
+import 'package:mark7/common/const/colors.dart';
 import 'package:mark7/common/layout/default_layout.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -8,21 +9,59 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          _Title(),
-          CustomTextFormField(
-            hintText: 'Enter your email',
-            onChanged: (String value) {},
-          ),
-          const SizedBox(height: 10),
-          CustomTextFormField(
-            hintText: 'Enter your password',
-            obscureText: true,
-            onChanged: (String value) {},
-          ),
-        ],
+      child: SafeArea(
+        top: true,
+        bottom: false,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const _Title(),
+            const _SubTitle(),
+            Padding(
+              padding: const EdgeInsets.only(top: 20, bottom: 20),
+              child: Image.asset(
+                "asset/img/misc/ai_logo.png",
+                height: MediaQuery.of(context).size.width / 1,
+              ),
+            ),
+            CustomTextFormField(
+              hintText: 'Enter your email',
+              onChanged: (String value) {},
+            ),
+            const SizedBox(height: 10),
+            CustomTextFormField(
+              hintText: 'Enter your password',
+              obscureText: true,
+              onChanged: (String value) {},
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: PRIMARY_COLOR,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text(
+                'Login',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text("sign up",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: PRIMARY_COLOR,
+                  )),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -33,6 +72,27 @@ class _Title extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("환영합니다.");
+    return const Text(
+      "Welcome!",
+      style: TextStyle(
+        fontSize: 34,
+        fontWeight: FontWeight.w500,
+        color: Colors.black,
+      ),
+    );
+  }
+}
+
+class _SubTitle extends StatelessWidget {
+  const _SubTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Use your email to login\nInvite to the AI world',
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+          color: BODY_TEXT_COLOR,
+        ));
   }
 }
