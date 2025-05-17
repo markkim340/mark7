7d7030f2dc28d6bf0ddf8f539a90aaf545552aa5
mark_kim <mark340v@gmail.com>
2025-05-17 14:00:14 +0900

    feat: add keyboard dismiss on drag & fix styles

--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -9,58 +9,65 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-      child: SafeArea(
-        top: true,
-        bottom: false,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const _Title(),
-            const _SubTitle(),
-            Padding(
-              padding: const EdgeInsets.only(top: 20, bottom: 20),
-              child: Image.asset(
-                "asset/img/misc/ai_logo.png",
-                height: MediaQuery.of(context).size.width / 1,
-              ),
-            ),
-            CustomTextFormField(
-              hintText: 'Enter your email',
-              onChanged: (String value) {},
-            ),
-            const SizedBox(height: 10),
-            CustomTextFormField(
-              hintText: 'Enter your password',
-              obscureText: true,
-              onChanged: (String value) {},
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: PRIMARY_COLOR,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
+      child: SingleChildScrollView(
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        child: SafeArea(
+          top: true,
+          bottom: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const _Title(),
+                const _SubTitle(),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20, bottom: 20),
+                  child: Image.asset(
+                    "asset/img/misc/ai_logo.png",
+                    height: MediaQuery.of(context).size.width / 1,
+                  ),
                 ),
-              ),
-              child: const Text(
-                'Login',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.white,
+                CustomTextFormField(
+                  hintText: 'Enter your email',
+                  onChanged: (String value) {},
                 ),
-              ),
+                const SizedBox(height: 10),
+                CustomTextFormField(
+                  hintText: 'Enter your password',
+                  obscureText: true,
+                  onChanged: (String value) {},
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: PRIMARY_COLOR,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text("sign up",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: PRIMARY_COLOR,
+                      )),
+                ),
+              ],
             ),
-            TextButton(
-              onPressed: () {},
-              child: const Text("sign up",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                    color: PRIMARY_COLOR,
-                  )),
-            ),
-          ],
+          ),
         ),
       ),
     );
