e8b6635fdc3796122743bf7b6f1f0e8c52bf6044
mark_kim <mark340v@gmail.com>
2025-06-03 19:12:01 +0900

    feat : Add scroll paginate event to RestaurantScreen

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -5,16 +5,46 @@ import 'package:mark7/restaurant/component/restaurant_card.dart';
 import 'package:mark7/restaurant/provider/restaurant_provider.dart';
 import 'package:mark7/restaurant/view/restaurant_detail_screen.dart';
 
-class RestaurantScreen extends ConsumerWidget {
+class RestaurantScreen extends ConsumerStatefulWidget {
   const RestaurantScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  void _scrollListener() {
+    // Check if the user has scrolled to the bottom of the list
+    if (_scrollController.offset >
+        _scrollController.position.maxScrollExtent - 300) {
+      // Fetch more data when the user reaches the end of the list
+      ref.read(restaurantProvider.notifier).paginate(fetchMore: true);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final data = ref.watch(restaurantProvider);
 
+    // If the data is loading, show a loading indicator
     if (data is CursorPaginationLoading) {
       return const Center(
         child: CircularProgressIndicator(),
+      );
+    }
+
+    // If there is an error, show the error message
+    if (data is CursorPaginationError) {
+      return Center(
+        child: Text(data.message),
       );
     }
 
@@ -23,8 +53,23 @@ class RestaurantScreen extends ConsumerWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: ListView.separated(
-        itemCount: cp.data.length,
+        controller: _scrollController,
+        itemCount: cp.data.length + 1,
         itemBuilder: (_, index) {
+          if (index == cp.data.length) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 8.0,
+                vertical: 16.0,
+              ),
+              child: Center(
+                child: data is CursorPaginationFetchingMore
+                    ? const CircularProgressIndicator()
+                    : const Text('No more data to load'),
+              ),
+            );
+          }
+
           final pItem = cp.data[index];
 
           return GestureDetector(
