f90b06a337d84663de29742b83ca0cc1009026cc
mark_kim <mark340v@gmail.com>
2025-06-06 22:49:47 +0900

    feat : Add renderRatings, reting_card in restaurant_detail_screen

--- lib/rating/component/rating_card.dart
+++ lib/rating/component/rating_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mark7/common/const/colors.dart';
 import 'package:collection/collection.dart';
+import 'package:mark7/rating/model/rating_model.dart';
 
 class RatingCard extends StatelessWidget {
   final ImageProvider avatarImage;
@@ -17,6 +18,18 @@ class RatingCard extends StatelessWidget {
     required this.email,
     required this.content,
   });
+
+  factory RatingCard.fromModel({
+    required RatingModel model,
+  }) {
+    return RatingCard(
+      avatarImage: NetworkImage(model.user.imageUrl),
+      images: model.imgUrls.map((el) => Image.network(el)).toList(),
+      rating: model.rating,
+      email: model.user.username,
+      content: model.content,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
