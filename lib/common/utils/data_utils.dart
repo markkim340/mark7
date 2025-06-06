f0a49f90b522501db929d2fde9a5eddf3965733a
mark_kim <mark340v@gmail.com>
2025-06-06 16:54:49 +0900

    feat : Add rating_model and user_model

--- lib/common/utils/data_utils.dart
+++ lib/common/utils/data_utils.dart
@@ -4,4 +4,8 @@ class DataUtils {
   static String pathToUrl(String value) {
     return 'http://$ip/$value';
   }
+
+  static List<String> listPathsToUrls(List<String> values) {
+    return values.map((e) => pathToUrl(e)).toList();
+  }
 }
